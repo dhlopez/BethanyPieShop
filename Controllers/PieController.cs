27ec3dbab56c097861e3983c5069a4450bc02bf0
dhlopez <dhelaman@hotmail.com>
2022-08-30 23:15:22 -0400

    Migrations + Home page

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -26,5 +26,16 @@ namespace BethanyPieShop.Controllers
             piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+
+        }
+
     }
 }
