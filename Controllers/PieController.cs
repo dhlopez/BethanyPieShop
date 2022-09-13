c7ced573a3b3bdfd292df7e8c60948ceb4038761
dhlopez <dhelaman@hotmail.com>
2022-09-12 23:19:59 -0400

    Support for orders

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -19,13 +19,13 @@ namespace BethanyPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "Cheese cakes";
-            return View(piesListViewModel);
-        }
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    piesListViewModel.CurrentCategory = "Cheese cakes";
+        //    return View(piesListViewModel);
+        //}
 
         public IActionResult Details(int id)
         {
@@ -37,5 +37,27 @@ namespace BethanyPieShop.Controllers
 
         }
 
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category).ToString();
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
+        }
     }
 }
